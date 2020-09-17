bb3b9a1fd81f6eed6479d68df0add80ccd490c2e
krishnabarman <krishna29bd@gmail.com>
2020-09-17 23:04:36 +1000

    updated to exclude /.idea directory

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('books', BookController::class);
+//Route::post('/books',[BookController::class,'store']);
+//Route::patch('/books',[BookController::class,'update']);
