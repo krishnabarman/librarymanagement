6fe5b22fde47ab0d7df4fdee64b5341d26bd1b22
krishnabarman <krishna29bd@gmail.com>
2020-09-24 23:40:39 +1000

    added name and dob validation for authors

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -88,7 +88,7 @@ class AuthorController extends Controller
 
         return request()->validate([
             'name' => 'required',
-            'dob' => 'required'
+            'dob' => 'required'           
 
         ]);
     }
--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -17,20 +17,44 @@ class AuthorManagementTest extends TestCase
      */
     public function an_author_can_be_created()
     {
-        $this->withoutExceptionHandling();
-        $this->post('/authors', [
-            'name' => 'Krishna',
-            'dob' => '07/19/1979'
-        ]);
+        $this->post('/authors', $this->data());
 
         $author = Author::all();
 
-        $this->assertCount(1,$author);
+        $this->assertCount(1, $author);
 
         // check if the date is Carbon instanced
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
 
         $this->assertEquals('19/07/1979', $author->first()->dob->format('d/m/Y'));
+    }
 
+    /**
+     * @test
+     */
+    public function a_name_is_required(){
+
+       $response = $this->post('/authors', array_merge($this->data(),['name' => '']));
+       $response->assertSessionHasErrors('name');
+
+    }
+
+        /**
+     * @test
+     */
+    public function a_dob_is_required(){
+
+        $response = $this->post('/authors', array_merge($this->data(),['dob' => '']));
+        $response->assertSessionHasErrors('dob');
+ 
+     }
+
+    private function data()
+    {
+
+        return [
+            'name' => 'Krishna',
+            'dob' => '07/19/1979'
+        ];
     }
 }
