cc898f950e68c93cc0581d8dc2b731f395277b3a
krishnabarman <krishna29bd@gmail.com>
2020-09-29 14:37:40 +1000

    updated navbar links

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,6 +8,10 @@ use PhpParser\Node\Expr\FuncCall;
 
 class BookController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
